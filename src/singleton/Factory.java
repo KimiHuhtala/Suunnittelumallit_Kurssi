858baaf50afdb80abdd65950ab0ba83a41552e49
Kimi <kimi.huhtala@icloud.com>
2024-02-06 17:51:49 +0200

    Tehtävät 4 ja 5

--- src/singleton/Factory.java
+++
@@ -1,9 +0,0 @@
-package singleton;
-
-
-public interface Factory {
-    public abstract String createEmail();
-    public abstract String createBrowser();
-    public abstract String createTools();
-
-}
