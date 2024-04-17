777960d96289720d3745bb16e86260778f16c94a
Kimi <kimi.huhtala@icloud.com>
2024-04-17 11:45:04 +0300

    Prototype Task

---
+++ src/Prototype/Clock.java
@@ -0,0 +1,23 @@
+package Prototype;
+
+public abstract class Clock implements Cloneable {
+    protected String clockType;
+
+    abstract void setHour(int hour);
+    abstract void setMinute(int minute);
+
+    public String getClockType() {
+        return clockType;
+    }
+
+    @Override
+    public Object clone() {
+        Object clone = null;
+        try {
+            clone = super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return clone;
+    }
+}
